ea4845e3af60092994320394ec4f725bf0ebaaa1
Mahdi Ruiz <RuizMahdi@gmx.com>
2023-11-24 19:26:28 -0700

    Update embed.js

--- assets/scripts/embed.js
+++ assets/scripts/embed.js
@@ -1,3 +1,3 @@
-document.getElementById('oneads').src = "https://daniel4-scratch.is-a.dev/adverts/generate"
+document.getElementById('oneads').src = "https://mrdev88.github.io/one-ads/generate.html"
 document.getElementById('oneads').style = "border:none; width:728px; height:90px; overflow:hidden;"
 document.getElementById('oneads').scrolling  = "no"
